dbae4d8b80195703f1ebda34608155c7d7b16bc9
azychkov <azychkov@emdev.ru>
2021-01-26 13:56:44 +0700

    EL-169 First commit

---
+++ modules/gradebook/gradebook-web/src/main/java/ru/zychkov/training/gradebook/web/constants/GradebookPortletKeys.java
@@ -0,0 +1,11 @@
+package ru.zychkov.training.gradebook.web.constants;
+
+/**
+ * @author artem
+ */
+public class GradebookPortletKeys {
+
+	public static final String GRADEBOOK =
+		"ru_zychkov_training_gradebook_web_portlet_GradebookPortlet";
+
+}
